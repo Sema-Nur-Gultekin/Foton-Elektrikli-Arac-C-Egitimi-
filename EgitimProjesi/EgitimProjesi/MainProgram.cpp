2b1a4561b418fd917ad7f5e0349489de6e8cd944
Sema-Nur-Gultekin <semanurgultekin00@gmail.com>
2025-02-05 13:01:46 +0300

    Add files via upload

---
+++ EgitimProjesi/EgitimProjesi/MainProgram.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	string isim;
+	int yas = 20;
+	yas = 21;
+	isim = "Ali Kaya";
+
+	cout << "Merhaba" << endl << endl << "Ogrenci Adi - Soyadi : " << isim;
+	cout << "Ogrenci Yasi : " << yas;
+
+	cout << endl;
+	return 0;
+}
